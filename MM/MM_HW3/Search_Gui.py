2627b59afafb311976ed612e00e00848222d3a2f
ktapsyman <ktapsyman@gmail.com>
2017-12-11 20:35:05 +0800

    Base completed

--- MM/MM_HW3/Search_Gui.py
+++ MM/MM_HW3/Search_Gui.py
@@ -4,19 +4,10 @@ Environment:
 	OS : MacOS Sierra
 	Python : 2.7.10
 """
-import os
 from pylab import *
-
-import pickle
-
-from Tkinter import *
-from PIL import ImageTk, Image
-import tkMessageBox
-import tkFileDialog 
-from ttk import Frame, Button, Label, Style
-
 from Util import *
 
+TITLE = ["Rank", "Filename", "Image", "Metric", "Similarity"]
 
 class Example(Frame):
   
@@ -27,41 +18,80 @@ class Example(Frame):
 
 	def setAllImages(self, Images):
 		self.allImages = Images
-		Images[0][1].show()
-		print l2Norm(Images[0][1].colorHistogram, Images[1][1].colorHistogram)
-		for i in range(10):
-			continue
+		"""
+		This image is a bilevel image! I've dealt with it 
+		test = searchImageByName("ukbench00472.jpg", Images)
+		print(r)
+		print(g)
+		print(b)
+		print test.getColorHistogram()
+		"""
 	
 	def cleanUp(self):
 		for img in self.allImages:
-			img[1].close()
+			img.close()
 
 	def initUI(self):
 	  
 		self.parent.title("HW3") 
 
-		self.pack(fill=BOTH, expand=1)
+		self.pack(fill=BOTH, expand=True)
 
-		Button(self, text = "Select File", command = openFile).grid(row=0, column=0, pady=5)
+		Label(self, font=("Courier", 14, "bold"), text = "Select File: ").grid(row=0, column=0, pady=5)
+		Button(self, text = "Click to select file", command = lambda : openFile(self)).grid(row=0, column=1, pady=5)
 		self.fileName = StringVar()
-		Label(self, textvariable=self.fileName).grid(row=0, column=1, columnspan=2, pady=5, sticky=W)
+		Label(self, textvariable=self.fileName, font=("Courier", 14)).grid(row=0, column=1, columnspan=2, pady=5, sticky=W)
 
-		Label(self, text = "Select Mode: ").grid(row=1, column=0, pady=5)
+		Label(self, text = "Select Mode: ", font=("Courier", 14, "bold")).grid(row=1, column=0, pady=5)
 		mode = StringVar(self)
 		mode.set("Q1-ColorHistogram")
-		om = OptionMenu(self, mode, "Q1-ColorHistogram", "Q2-ColorLayout", "Q3-SIFT Visual Words", "Q4-Visual Words using stop words")
+		om = OptionMenu(self, mode, "Q1-ColorHistogram", "Q2-ColorLayout", "Q3-SIFT Visual Words", "Q4-Visual Words using stop words" )
 		om.grid(row=1, column=1, pady=5, sticky=W)
 
-		Button(self, text = "SEARCH", command = lambda: startSearching(self.fileName.get(),mode.get())).grid(row=3, column=0, pady=5)
-
+		Button(self, text = "SEARCH!", command = lambda: startSearching(self, self.fileName.get(),mode.get())).grid(row=3, column=1, pady=5)
+		
+		for col in range(5):
+			titleLbl = Label(self, text=TITLE[col], font=("Courier", 14, "bold")).grid(row=4, column=col, padx=50)
+		
 		self.images = []
-		for i in range(10):
-			self.images.append(Label(self, text="Test"+str(i)))
-			self.images[i].grid(row=i/5+4, column=i%5, pady=50)
- 
+		for rowCount in range(10):
+			for colCount in range(5):
+				self.images.append(Label(self, text="Test row="+str(rowCount) + " col=" + str(colCount)))
+				self.images[rowCount*5+colCount].grid(row=rowCount+5, column=colCount, pady=20, padx=5)
+	
+	def updateImgList(self, imgList):
+		for rowCount in range(10):
+			for colCount in range(5):
+				self.images[rowCount*5+colCount].configure(image=None)
+		
+		
+def searchImageByName(fileName, imgList):
+	print(fileName)
+	for img in imgList:
+		if img.getFileName() == fileName:
+			return img
+	return None
 
-def startSearching (fileName, mode):
+def getTop10SimilarColorHist(img, imgList):
+	targetHist = img.getColorHistogram()
+	print targetHist.shape
+	print imgList[10].getColorHistogram().shape
+	for image in imgList:
+		print image.getFileName()
+		print image.getColorHistogram().shape
+	top10ColorHist = [(image, l2Norm(targetHist, image.getColorHistogram())) for image in imgList]#.sort(key=lambda x : x[1])
+	return top10ColorHist
+
+def startSearching (app, fileName, mode):
+	imgList = []
+	targetImg = searchImageByName(fileName, app.allImages)
 	if mode == "Q1-ColorHistogram":
+		if 0 != len(targetImg.getMetricResult(mode)):
+			imgList	= targetImg.getMetricResult[mode]
+		else:
+			imgList = getTop10SimilarColorHist(targetImg, app.allImages)
+			targetImage.setMetricResult(mode, imgList)
+			print imgList
 		print mode
 	elif mode == "Q2-ColorLayout":
 		print mode
@@ -70,13 +100,15 @@ def startSearching (fileName, mode):
 	elif mode == "Q4-Visual Words using stop words":
 		print mode
 	
+	app.updateImgList(imgList)
+	
 
 if __name__ == '__main__':
 	root = Tk()
-	size = 220, 220
+	size = 720, 1024
 
 	app = Example(root)
-	app.setAllImages([(img, customizedImage(Image.open("./dataset/"+img))) for img in os.listdir("./dataset") if ".jpg" in img])
+	app.setAllImages([customizedImage(img, Image.open("./dataset/"+img)) for img in os.listdir("./dataset") if ".jpg" in img])
 	root.geometry("1024x720")
 	root.mainloop()
 	app.cleanUp()
--- MM/MM_HW3/Util.py
+++ MM/MM_HW3/Util.py
@@ -4,28 +4,62 @@ Environment:
 	OS : MacOS Sierra
 	Python : 2.7.10
 """
-from PIL import Image
+import os
 import numpy as np
 
+from Tkinter import *
+import tkFileDialog 
+import tkMessageBox
+from ttk import Frame, Button, Label, Style
+
+from PIL import ImageTk, Image
+
 class customizedImage(object):
-	def __init__(self, img):
+	def __init__(self, fileName, img):
 		self._img = img
-		self.colorHistogram = np.array(img.histogram())
-		self.colorLayout = getColorLayout(img)
-		self.SIFT = None #TODO
+		self._fileName = fileName
+		self._colorHistogram = np.array(img.histogram())
+		self._colorLayout = getColorLayout(img)
+		self.MetricDic = {"Q1-ColorHistogram":[], "Q2-ColorLayout":[], "Q3-SIFT Visual Words":[], "Q4-Visual Words using stop words":[]}
+		self.SIFTVisualWords = None #TODO
+		self.SIFTWithStopWords = None #TODO
 	def show(self):
 		self._img.show()
 
 	def close(self):
 		self._img.close()
+	
+	def getFileName(self):
+		return self._fileName
+	
+	def getColorHistogram(self):
+		return self._colorHistogram
 
-def openFile ():
-	fileName = tkFileDialog.askopenfilename(initialdir = "./dataset")
-	app.fileName.set(fileName)
+	def getColorLayout(self):
+		return self._colorLayout
+	
+	def getSIFTVisualWords(self):
+		return self.SIFTVisualWords	
+	
+	def getSIFTWithStopWords(self):
+		return self.SIFTWithStopWords
+	
+	def getMetricResult(self, metric=""):
+		return self.MetricDic[metric]	
+	
+	def setMetricResult(self, metricResult, metric="" ):#Top 10 only
+		self.MetricDic[metric] = metricResult
 
 def getColorLayout(img):
 	#TODO
 	return None
 
+def openFile (app):
+	fileName = tkFileDialog.askopenfilename(initialdir = "./dataset")
+	app.fileName.set(os.path.split(fileName)[1])
+
+
 def l2Norm(vec1, vec2):
+	if 256 == vec1.shape[0] or 256 == vec2.shape[0]:
+		print 256
 	return np.linalg.norm(vec2-vec1)
