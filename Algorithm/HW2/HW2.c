418b24c86ab22704d971bf9279021fb3da617911
ktapsyman <ktapsyman@gmail.com>
2018-05-14 11:02:52 +0800

    Algo HW2 30%

--- Algorithm/HW2/HW2.c
+++ Algorithm/HW2/HW2.c
@@ -3,12 +3,13 @@
 #include <stdlib.h>
 
 #ifndef SAFE_FREE  
-#define SAFE_FREE(Ptr) if(Ptr){\  
-                                  free(Ptr);\  
+#define SAFE_FREE(Ptr) if(Ptr){\
+                                  free(Ptr);\
 								  Ptr = NULL;\
-							  }  
+							  }
 #endif  
 
+int Comparison(const void *a, const void *b);
 int **GetHeightFromStdin(int M, int N);
 int CalcMaximumGrades(int **School);
 
@@ -20,16 +21,29 @@ int main(int argc, char **atgv)
 		printf("Input error : invalid M or N\n");
 		exit(0);
 	}
+	printf("M = %d, N = %d\n", M, N);
 	int **Students = GetHeightFromStdin(M, N);
 	
 	int Grade = 0;
 	for(; Grade < M; ++Grade)
+	{
+		int StudentIndex = 0;
+		for(; StudentIndex < N; ++StudentIndex)
+		{
+			printf("%d ", Students[Grade][StudentIndex]);
+		}
+		printf("\n");
 		SAFE_FREE(Students[Grade]);
+	}
 	SAFE_FREE(Students);
 
 	return 0;
 }
 
+int Comparison(const void *a, const void *b)
+{
+	return *(int *)a - *(int *)b;
+}
 
 int **GetHeightFromStdin(int M, int N)
 {
@@ -43,11 +57,13 @@ int **GetHeightFromStdin(int M, int N)
 		for(; StudentIndex < N; ++StudentIndex)
 		{
 			if(1 != scanf("%d", &Students[Grade][StudentIndex]))
-				print("Error occured\n");
+				printf("Error occured\n");
 			printf("%d ", Students[Grade][StudentIndex]);
 		}
+		qsort(Students[Grade], N, sizeof(int), Comparison);
 		printf("\n");
 	}
+	printf("==============\n");
 	return Students;
 }
 
